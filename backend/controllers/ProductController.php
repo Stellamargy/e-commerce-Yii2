a021608d813ef86fa977822c72422a5992cfedc6
stlmargy <stellahmargy0211@gmail.com>
2025-02-11 14:21:02 +0300

    creates view , index method and index view for product entity

--- backend/controllers/ProductController.php
+++ backend/controllers/ProductController.php
@@ -2,26 +2,65 @@
 
 namespace backend\controllers;
 
+use Yii;
+use common\models\Product;
+use yii\data\ActiveDataProvider;
+use yii\web\NotFoundHttpException;
+
 class ProductController extends \yii\web\Controller
 {
     public function actionCreate()
     {
+
         return $this->render('create');
     }
 
     public function actionIndex()
     {
-        return $this->render('index');
+        $productsData=new ActiveDataProvider([
+            'query'=>Product::find(),
+            'pagination'=>[
+                'pageSize'=>10,
+            ],
+            'sort' => [
+            'defaultOrder' => ['id' => SORT_DESC], 
+        ],
+        ]);
+      
+        return $this->render('index', ['products' => $productsData]);
     }
 
-    public function actionUpdate()
+    public function actionUpdate($id)
     {
-        return $this->render('update');
+
+        $product = Product::findOne($id);
+
+        if ($product === null) {
+            throw new NotFoundHttpException('Product not found.');
+        }
+
+
+        if (Yii::$app->request->isPost) {
+
+            if ($product->load(Yii::$app->request->post()) && $product->validate()) {
+
+                if ($product->save()) {
+                    Yii::$app->session->setFlash('success', 'Product updated successfully!');
+                    return $this->redirect(['view', 'id' => $product->id]);
+                }
+            }
+        }
+
+
+        return $this->render('update', ['product' => $product]);
     }
 
-    public function actionView()
+    public function actionView($id)
     {
-        return $this->render('view');
+        $product = Product::findOne($id);
+        if ($product->id === null) {
+            throw new NotFoundHttpException("Product not found");
+        }
+        return $this->render('view', ['product' => $product]);
     }
-
 }
--- backend/views/product/index.php
+++ backend/views/product/index.php
@@ -1,9 +1,41 @@
 <?php
 /** @var yii\web\View $this */
+use yii\helpers\Html;
+use yii\grid\GridView;
+$this->title ="Products";
+$this->params['breadcrumbs'][] = $this->title; 
 ?>
-<h1>product/index</h1>
 
-<p>
-    You may change the content of this page by modifying
-    the file <code><?= __FILE__; ?></code>.
-</p>
+<div class="product-index">
+
+    <h1><?= Html::encode($this->title) ?></h1>
+
+    <p>
+        <?= Html::a('Create Product', ['create'], ['class' => 'btn btn-success']) ?>
+    </p>
+
+    <?= GridView::widget([
+        'dataProvider' => $products,
+        'columns' => [
+            ['class' => 'yii\grid\SerialColumn'], 
+            'product_name',
+            'product_price',
+            'product_quantity',
+            'product_description', 
+            [
+                'attribute'=>'product_category_id',
+                'label'=>'Category',
+                'value'=>function($model){
+                    return $model->productCategory->category_name ?? '(Not Set)';
+                }
+
+            ],
+
+            [
+                'class' => 'yii\grid\ActionColumn', // Default edit/view/delete buttons
+                // 'template' => '{view} {update} {delete}', // Customize buttons
+            ],
+        ],
+    ]); ?>
+</div>
+
